d6e074adb571163e4dbc0940b7d8e8fdb0ed9cb3
Christy Lam <christy.pie2003@gmail.com>
2025-12-09 23:51:43 -0500

    Update HelloWorld.jsx

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class HelloWorld extends Component {
     render() {
         return (
-        	/*'TODO (HelloWorld): Add a line such that "Hello World!" is displayed on your webpage!*/
+            <h1>Hello World!</h1>
         );
     }
 }
