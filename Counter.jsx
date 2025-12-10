0f719c15821438e160205be18487b99d2aeee5da
Christy Lam <christy.pie2003@gmail.com>
2025-12-09 23:41:08 -0500

    tasks

--- Counter.jsx
+++ Counter.jsx
@@ -10,19 +10,16 @@ class Counter extends Component {
     }
 
     incrementCount = () => {
-        /* TODO (Counter): Use setState() to modify the count. Here’s an example:
-            this.setState({
-                keyToChange: valueToChangeTo
-            });
-        */
+        this.setState({
+            count: this.state.count + 1
+        });
     }
   
     render() {
         return (
             <div className="counter">
-                /*TODO (Counter): display the value of count*/
-
-                /*TODO (Counter): add a button that calls incrementCount() when clicked*/
+                <h1>{this.state.count}</h1>
+                <button onClick={this.incrementCount}>Increase</button>
             </div>
         );
     }
